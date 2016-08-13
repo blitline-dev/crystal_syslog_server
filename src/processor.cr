0d7e4a91a3c2c7ec02a58ed0d953909283814c4d
Jason Malcolm <jaciones@hotmail.com>
2016-08-13 01:07:54 +0000

    Workeirng

--- src/processor.cr
+++ src/processor.cr
@@ -1,27 +1,45 @@
-require "./frame"
+# ["<134>Aug", "13", "00:27:32", "nat", "shake@nat", "Tell", "him", "I'll", "send", "Duke", "Edmund", "to", "the", "Tower-\\n"]
 
 class Processor
-  def initialize()
-    @wants_close = false
+	LOOKUP_HASH = { "Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" => 4, "May" => 5, "Jun" => 6 , "Jul" => 7 , "Aug" => 8, "Sep" => 9,  "Oct" => 10, "Nov" => 11, "Dev" => 12}
+
+  def process(data)
+		puts split_data(data).inspect
   end
 
-  def close
-    @wants_close = true
-  end
+	def split_data(data : String)
+		segments = data.split(" ")
+		type_month = segments[0]
+		hostname = segments[3]
+    tag = segments[4]
+		log_type = ""
+    month = ""
 
-  def process(input, output, error = STDERR)
-    must_close = true
+		type_month.match(/<([0-9]*)>/) do |m|
+			log_type = m[1] if m
+		end
 
-    begin
-      until @wants_close
-        dat = Frame.from_io(input)
-        # EOF
-        output.flush
-      end
-    rescue ex : Errno
-      # IO-related error, nothing to do
-    ensure
-      input.close if must_close
+		type_month.match(/>(.*)/) do |m|
+			month = m[1] if m
     end
+
+		time = build_date(month, segments[1], segments[2])
+		output = Hash(String, String).new
+
+    output["log_local_time"] = time.to_s
+		output["ingestion_time"] = Time.now.to_s
+		output["log_type"] = log_type
+		output["body"] = segments[5..-1].join(" ").strip
+		return output
   end
+
+	def build_date(month, day, time)
+		year = Time.now.year
+		hour_minute_seconds = time.split(":")
+    built_time = Time.new(year, LOOKUP_HASH[month[0..2].camelcase], day.to_i, hour_minute_seconds[0].to_i, hour_minute_seconds[1].to_i, hour_minute_seconds[2].to_i)
+		return built_time		
+	end
+
+
 end
+
--- src/tcp.cr
+++ src/tcp.cr
@@ -4,8 +4,6 @@ require "./processor.cr"
 require "./frame"
 class Tcp
 
-  @wants_close = false
-
   def self.new(port)
     new("0.0.0.0", port)
   end
@@ -13,27 +11,34 @@ class Tcp
   def initialize(@host : String, @port : Int32)
   end
 
+	def reader(socket : TCPSocket, processor : Processor)
+    while data = socket.gets
+      processor.process(data)
+    end
+	end
+
+	def spawn_listener(socket_channel : Channel, processor : Processor)
+		3.times do
+      spawn do
+        loop do
+          socket = socket_channel.receive
+					reader(socket, processor)
+          socket.close
+        end
+      end
+    end
+  end
+
   def listen
 		ch = Channel(TCPSocket).new
 		server = TCPServer.new(@host, @port)
-
-		3.times do
-  		spawn do
-    		loop do
-      		socket = ch.receive
-      		while data = socket.gets
-						puts data
-					end
-      		socket.close
-    		end
-  		end
-		end
+		processor = Processor.new
 		
+		spawn_listener(ch, processor)
 		loop do
   		socket = server.accept
   		ch.send socket
-		end
- 
+		end 
   end
 
 end
