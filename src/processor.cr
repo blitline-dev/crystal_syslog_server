0e6b95106c45802a1fc047942eda842de0751916
Jason Malcolm <jaciones@gmail.com>
2016-09-01 14:11:21 -0700

    Fix for funky syslog format string

--- src/processor.cr
+++ src/processor.cr
@@ -4,86 +4,92 @@ require "./type_table"
 #  "body" => "ANTONY. Moon and stars!", "facility" => "local0", "severity" => "6"}
 
 class Processor
-	LOOKUP_HASH = { "Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" => 4, "May" => 5, "Jun" => 6 , "Jul" => 7 , "Aug" => 8, "Sep" => 9,  "Oct" => 10, "Nov" => 11, "Dev" => 12}
+  LOOKUP_HASH = { "Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" => 4, "May" => 5, "Jun" => 6 , "Jul" => 7 , "Aug" => 8, "Sep" => 9,  "Oct" => 10, "Nov" => 11, "Dev" => 12}
   
 
   def process(data : String) : Hash(String, String) | ::Nil
-		begin
-			hash = split_data(data)
-			return hash
-		rescue ex
-			puts ex.message
-			puts ex.callstack
-		end
-		return nil
+    begin
+      hash = split_data(data)
+      return hash
+    rescue ex
+      puts ex.message
+      puts ex.callstack
+    end
+    return nil
   end
 
-	def split_data(data : String) : Hash(String, String)
-		segments = data.split(" ", 8)
-		type_month = segments[0]
-		log_type = ""
+  def split_data(data : String) : Hash(String, String)
+    segments = data.split(" ", 8).reject! {|s| s.nil? || s.empty?}
+    if segments.nil?
+      puts "Illegal values for splitting"
+      puts segments.inspect
+      puts "----------------------------"
+      return Hash(String,String).new 
+    end
+    type_month = segments[0]
+    log_type = ""
     month = ""
-		hostname = ""
-		tag = ""
-		proc_id = ""
-		msg_id = ""
-		structured_data = ""
+    hostname = ""
+    tag = ""
+    proc_id = ""
+    msg_id = ""
+    structured_data = ""
 
-		body_start = 5
+    body_start = 5
 
-		version_one = false
-		type_month.match(/<([0-9]*)>(1?)/) do |m|
-			log_type = m[1] if m
-			version_one = true if m[2] == "1"
-		end
+    version_one = false
+    type_month.match(/<([0-9]*)>(1?)/) do |m|
+      log_type = m[1] if m
+      version_one = true if m[2] == "1"
+    end
 
-		if version_one
-			iso_date_string = segments[1]
-			begin
-				time = Time.parse(iso_date_string, "%Y-%m-%dT%H:%M:%S", Time::Kind::Utc)
-			rescue bs
-				puts bs.inspect_with_backtrace
-				time = Time.now
-			end
-			hostname = segments[2]
-	    tag = segments[3]
+    if version_one
+      iso_date_string = segments[1]
+      begin
+        time = Time.parse(iso_date_string, "%Y-%m-%dT%H:%M:%S", Time::Kind::Utc)
+      rescue bs
+        puts bs.inspect_with_backtrace
+        time = Time.now
+      end
+      hostname = segments[2]
+      tag = segments[3]
 
-			proc_id = segments[4]
-			msg_id = segments[5]
-			structured_data = segments[6]
+      proc_id = segments[4]
+      msg_id = segments[5]
+      structured_data = segments[6]
 
-			body_start = 7
-		else
-			type_month.match(/>(.*)/) do |m|
-				month = m[1] if m
-	    end
-			hostname = segments[3]
-	    tag = segments[4]
-			body_start = 5
-			time = build_date(month, segments[1], segments[2])
-		end
+      body_start = 7
+    else
+      type_month.match(/>(.*)/) do |m|
+        month = m[1] if m
+      end
+      hostname = segments[3]
+      tag = segments[4]
+      body_start = 5
+      time = build_date(month, segments[1], segments[2])
+    end
 
-		output = Hash(String, String).new
+    output = Hash(String, String).new
     output["log_local_time"] = time.to_s("%s")
-		output["host"] = hostname
+    output["host"] = hostname
     output["tag"] = tag
-		output["proc_id"] = proc_id
-		output["msg_id"] = msg_id
-		output["structured_data"] = structured_data
-		output["suid"] = SecureRandom.urlsafe_base64(8)
-  	output["ingestion_time"] = Time.now.to_s("%s")
-		output["body"] = segments[body_start..-1].join(" ").strip
-		fac_sev = TypeTable.define(log_type.to_i)
+    output["proc_id"] = proc_id
+    output["msg_id"] = msg_id
+    output["structured_data"] = structured_data
+    output["suid"] = SecureRandom.urlsafe_base64(8)
+    output["ingestion_time"] = Time.now.to_s("%s")
+    output["body"] = segments[body_start..-1].join(" ").strip
+    fac_sev = TypeTable.define(log_type.to_i)
     output["facility"] = fac_sev[1].to_s
     output["severity"] = fac_sev[0].to_s
-		return output
+    return output
   end
 
-	def build_date(month : String, day : String , time : String)
-		built_time = ""
-		year = Time.now.year
-		hour_minute_seconds = time.split(":")
+  def build_date(month : String, day : String , time : String)
+    built_time = ""
+    year = Time.now.year
+    hour_minute_seconds = time.split(":")
     built_time = Time.new(year, LOOKUP_HASH[month[0..2].camelcase], day.to_i, hour_minute_seconds[0].to_i, hour_minute_seconds[1].to_i, hour_minute_seconds[2].to_i)
-		return built_time
-	end
+    return built_time
+  end
 end
