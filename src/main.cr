d2f7b6b96a2b7aa8a75964a039be2f79e0111131
Blitline Developers <jason@blitline.com>
2017-07-14 20:58:25 -0700

    Revert "Added TLS terminator"
    This reverts commit 8e2a6122981ab2e1a0e1ca91ba963d623a82fc4b.

--- src/main.cr
+++ src/main.cr
@@ -1,10 +1,9 @@
 require "./tcp.cr"
-require "./downloader.cr"
 
 port = ENV["CL_TCP_PORT"]? || "6768"
 stats_port = ENV["CL_STATS_TCP_PORT"]? || "6770"
 cl_dir = ENV["CL_BASE_DIR"]? || "/var/log/commonlogs"
-listen = ENV["CL_LISTEN"]? || "127.0.0.1"
+listen = ENV["CL_LISTEN"]? || "0.0.0.0"
 debug = ENV["CL_DEBUG"]?.to_s == "true"
 
 debug_type = 0
@@ -12,11 +11,10 @@ if ENV["CL_DEBUG_TYPE"]? && ENV["CL_DEBUG_TYPE"].to_i > 0
   debug_type = ENV["CL_DEBUG_TYPE"].to_i
 end
 
+
 puts "Starting syslog server"
 puts "TCP listening on #{listen}:#{port}"
 puts "Writing to #{cl_dir}"
-puts "Downloading certs"
-Downloader.download_certs
 puts "Logging TCP-IN" if debug_type == 1
 server = Tcp.new(listen, port.to_i, cl_dir, debug, debug_type)
 server.listen()
--- src/tcp.cr
+++ src/tcp.cr
@@ -1,7 +1,6 @@
 require "socket"
 require "./processor.cr"
 require "./action"
-require "openssl"
 class Tcp
 
   TOTAL_FIBERS = 200
@@ -18,41 +17,18 @@ class Tcp
     end
   end
 
-  def get_socket_data(socket : IO)
+  def get_socket_data(socket : TCPSocket)
     data = nil
     begin
-      raw_data = socket.gets(100)
-      while(raw_data)
-        data = data.nil? ? "" : data
-        data += raw_data
-        raw_data = socket.gets(100)
-      end
-
+      data = socket.gets
       puts data.to_s if @debug_type == 1
     rescue ex
       if @debug
-#        puts "From Socket Address:" + socket.remote_address.to_s if socket.remote_address
+        puts "From Socket Address:" + socket.remote_address.to_s if socket.remote_address
         puts ex.inspect_with_backtrace 
       end
     end
     return data
-  end
-
-  def ssl_reader(socket : IO, processor : Processor)
-    data = get_socket_data(socket)
-
-    puts "Recieved: #{data}" if @debug
-    while data
-      if data && data.size > 5
-        begin
-          formatted_data = processor.process(data)
-          @action.process(formatted_data)
-        rescue ex
-          p ex.message
-        end
-        data = get_socket_data(socket)
-      end
-    end
   end
 
 	def reader(socket : TCPSocket, processor : Processor)
@@ -106,53 +82,9 @@ class Tcp
   					@connections -= 1
           rescue ex
             p "Error in spawn_listener"
-            p ex.inspect_with_backtrace
+            p ex.message
           end
         end
-      end
-    end
-  end
-
-  def spawn_ssl_listener(socket_channel : Channel)
-    TOTAL_FIBERS.times do
-      spawn do
-        loop do
-          begin
-            socket = socket_channel.receive
-            ssl_reader(socket, @processor)
-          rescue ex
-            p "Error in spawn_listener"
-            p ex.inspect_with_backtrace
-          end
-        end
-      end
-    end
-  end
-
-  def spawn_ssl
-    # SSL Server
-    spawn do
-      # Should block, loop is just a retry
-      begin
-        channel = Channel(OpenSSL::SSL::Socket).new
-        spawn_ssl_listener(channel)
-
-        ssl_server = TCPServer.new(@host, @port - 1)
-        ssl_context = OpenSSL::SSL::Context::Server.new
-        ssl_context.private_key = "/etc/ssl/certs/server.key"
-        ssl_context.certificate_chain = "/etc/ssl/certs/server.crt"
-        ssl_context.verify_mode = OpenSSL::SSL::VerifyMode::NONE
-        spawn_ssl_listener(channel)
-        loop do
-          ssl_server.accept do |client|
-            ssl_socket = OpenSSL::SSL::Socket::Server.new(client, ssl_context)
-            channel.send ssl_socket
-          end
-        end
-      rescue ex
-        sleep 5
-        p "Error in spawn_ssl"
-        p ex.inspect_with_backtrace
       end
     end
   end
@@ -160,8 +92,8 @@ class Tcp
   def listen
 		ch = build_channel
 		server = TCPServer.new(@host, @port)
+
 		spawn_listener(ch)
-    spawn_ssl
     begin
   		loop do
     		socket = server.accept
@@ -176,7 +108,6 @@ class Tcp
   def build_channel
     Channel(TCPSocket).new
   end
-
 
 
 end
