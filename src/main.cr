62b301aa8f1408f94fd03659b2321a8f9d71a6dd
Blitline Developers <jason@blitline.com>
2017-07-18 17:20:05 -0700

    added socat proxy

--- src/main.cr
+++ src/main.cr
@@ -1,6 +1,7 @@
 require "./tcp.cr"
 
-port = ENV["CL_TCP_PORT"]? || "6768"
+socat_proxy_port = ENV["CL_TCP_PORT"]? || "6768"
+port = "1025"
 stats_port = ENV["CL_STATS_TCP_PORT"]? || "6770"
 cl_dir = ENV["CL_BASE_DIR"]? || "/var/log/commonlogs"
 listen = ENV["CL_LISTEN"]? || "0.0.0.0"
@@ -11,9 +12,15 @@ if ENV["CL_DEBUG_TYPE"]? && ENV["CL_DEBUG_TYPE"].to_i > 0
   debug_type = ENV["CL_DEBUG_TYPE"].to_i
 end
 
+# Make socat proxy
+fork do
+  io = IO::Memory.new
+  cmd = "socat TCP-LISTEN:#{socat_proxy_port},fork TCP:#{port}"
+  Process.run(cmd, shell: true, output: io)  
+end
 
 puts "Starting syslog server"
-puts "TCP listening on #{listen}:#{port}"
+puts "TCP listening on #{listen}:#{socat_proxy_port}"
 puts "Writing to #{cl_dir}"
 puts "Logging TCP-IN" if debug_type == 1
 server = Tcp.new(listen, port.to_i, cl_dir, debug, debug_type)
