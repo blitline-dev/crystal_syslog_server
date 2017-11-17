96d025c2d49f10dcf2244e27adeda343c38753e9
Jason GitKraken <jaciones@gmail.com>
2017-11-17 10:26:46 -0800

    OK

--- src/action.cr
+++ src/action.cr
@@ -5,76 +5,76 @@ require "./json_file_watcher"
 require "./string_mru"
 
 class Action
-	EVENT_CONFIG_NAME = "event_name"
-	EVENT_CONFIG_FIND = "find"
-	EVENT_CONFIG_FINDEX = "findex"
-	EVENT_CONFIG_REPLACE = "replace"
-	EVENT_BODY = "body"
-	TAG = "tag"
-  PRIV_TOKEN = ENV["CL_PRIV_TOKEN"]? || ""
-  PRIV_TOKEN_SIZE = PRIV_TOKEN.size + 1
-  SIZE_LIMIT = ENV.has_key?("CL_SIZE_LIMIT") ? ENV["CL_SIZE_LIMIT"].to_i64 : 70_000_000_000
+  EVENT_CONFIG_NAME    = "event_name"
+  EVENT_CONFIG_FIND    = "find"
+  EVENT_CONFIG_FINDEX  = "findex"
+  EVENT_CONFIG_REPLACE = "replace"
+  EVENT_BODY           = "body"
+  TAG                  = "tag"
+  PRIV_TOKEN           = ENV["CL_PRIV_TOKEN"]? || ""
+  PRIV_TOKEN_SIZE      = PRIV_TOKEN.size + 1
+  SIZE_LIMIT           = ENV.has_key?("CL_SIZE_LIMIT") ? ENV["CL_SIZE_LIMIT"].to_i64 : 70_000_000_000
 
-	def initialize(@file_root : String, @debug : Bool)
+  def initialize(@file_root : String, @debug : Bool)
     @sec = !PRIV_TOKEN.blank?
     @host_mru = StringMru.new(782400_i64, "#{@file_root}/hosts")
-		@file_watcher = FileWatcher.new
-		@events = Hash(String, JSON::Type).new
-		setup_configs(@file_watcher)
-		@file_manager = FileManager.new(@file_root, SIZE_LIMIT)
-		@channel = Channel::Buffered(SyslogData).new
-		build_channel(@channel)
+    @file_watcher = FileWatcher.new
+    @events = Hash(String, JSON::Type).new
+    setup_configs(@file_watcher)
+    @file_manager = FileManager.new(@file_root, SIZE_LIMIT)
+    @channel = Channel::Buffered(SyslogData).new
+    build_channel(@channel)
     puts "Secure = #{@sec}"
-	end
+  end
 
-	def open_file_count
-		@file_manager.open_file_count
-	end
+  def open_file_count
+    @file_manager.open_file_count
+  end
 
-	def setup_configs(file_watcher : FileWatcher)
-		# Events Config
+  def setup_configs(file_watcher : FileWatcher)
+    # Events Config
     json_watcher = JSONFileWatcher.new(file_watcher, @debug)
     r = Proc(Hash(String, JSON::Type), Nil).new { |x| @events = x["events"].as(Hash(String, JSON::Type)) }
-		filepath = "#{@file_root}/events.json"
-		data = "{ \"events\" : {} }"
-		File.write(filepath, data) unless File.exists?(filepath)
-    json_watcher.watch_file(filepath, r )
-	end
+    filepath = "#{@file_root}/events.json"
+    data = "{ \"events\" : {} }"
+    File.write(filepath, data) unless File.exists?(filepath)
+    json_watcher.watch_file(filepath, r)
+  end
 
-	def process(data : SyslogData | ::Nil)
-    puts "Action is processing #{data}" if @debug    
-		return unless data
+  def process(data : SyslogData | ::Nil)
+    puts "Action is processing #{data}" if @debug
+    return unless data
     if @sec
       unless data.tag.starts_with?(PRIV_TOKEN)
         raise "Illegal call, bad PRIV_TOKEN #{data}"
       end
       data.tag = data.tag.byte_slice(PRIV_TOKEN_SIZE)
     end
-		@channel.send data
-	end
+    @channel.send data
+  end
 
-	private def build_channel(ch : Channel(SyslogData))
-		spawn do
+  private def build_channel(ch : Channel(SyslogData))
+    spawn do
       loop do
-      	begin
+        begin
           data_hash = ch.receive
-				  if (data_hash.facility[0..4] == "local" || @sec) && !@file_manager.paused
-					  @file_manager.write_to_file(data_hash, nil) do |file|
-							handlle_output(data_hash, file)
-						end
-			      check_events(data_hash)
-				 	end
-	      rescue ex
-	      	p "Error in action:loop! #{data_hash.inspect}"
-      		p ex.message
-	      end
-     	end
+          if (data_hash.facility[0..4] == "local" || @sec) && !@file_manager.paused
+            @file_manager.write_to_file(data_hash, nil) do |file|
+              handlle_output(data_hash, file)
+            end
+            check_events(data_hash)
+          end
+        rescue ex
+          p "Error in action:loop! #{data_hash.inspect}"
+          p ex.message
+        end
+      end
     end
-	end
+  end
 
-	private def handlle_output(data_hash : SyslogData, file : File)
-		time = data_hash.ingestion_time
-		suid = data_hash.suid
+  private def handlle_output(data_hash : SyslogData, file : File)
+    time = data_hash.ingestion_time
+    suid = data_hash.suid
     host = data_hash.host
     tag = data_hash.tag
     body = data_hash.body
@@ -82,48 +82,47 @@ class Action
     @host_mru.add("#{host}:'#{tag}'")
     puts "Writing to file #{data_hash.to_s}" if @debug
     file.puts("#{time} #{suid} #{host} #{tag} #{body}")
-	end
+  end
 
-	private def check_events(data_hash)
-		tag = data_hash.tag
-		events_for_tag = @events[tag]?.as(Hash(String, JSON::Type) | Nil)
-		return unless events_for_tag
+  private def check_events(data_hash)
+    tag = data_hash.tag
+    events_for_tag = @events[tag]?.as(Hash(String, JSON::Type) | Nil)
+    return unless events_for_tag
 
-		puts "Checking Events..." if @debug
-		events_for_tag.keys.each do |event_key|
-			# Compiler workaround
-			b = events_for_tag[event_key].as(Hash(String, JSON::Type))
-			name = b.fetch(EVENT_CONFIG_NAME, nil).to_s
-			find = b.fetch(EVENT_CONFIG_FIND, nil).to_s
-			findex = b.fetch(EVENT_CONFIG_FINDEX, nil).to_s
-			replace = b.fetch(EVENT_CONFIG_REPLACE, nil).to_s
-			# End compiler workaround
-			handle_find( data_hash, find, name, false) unless find.empty?
-      handle_find( data_hash, find, name, true) unless findex.empty?
-		end
-		puts "... Done Checking Events" if @debug
-	end
+    puts "Checking Events..." if @debug
+    events_for_tag.keys.each do |event_key|
+      # Compiler workaround
+      b = events_for_tag[event_key].as(Hash(String, JSON::Type))
+      name = b.fetch(EVENT_CONFIG_NAME, nil).to_s
+      find = b.fetch(EVENT_CONFIG_FIND, nil).to_s
+      findex = b.fetch(EVENT_CONFIG_FINDEX, nil).to_s
+      replace = b.fetch(EVENT_CONFIG_REPLACE, nil).to_s
+      # End compiler workaround
+      handle_find(data_hash, find, name, false) unless find.empty?
+      handle_find(data_hash, find, name, true) unless findex.empty?
+    end
+    puts "... Done Checking Events" if @debug
+  end
 
-	private def handle_find(data_hash : SyslogData, find : String, name : String, is_regex : Bool)
-		if is_regex
-			regex = Regex.new(find)
-			
-			md = data_hash.body.match(regex)
-			if md
-				write_event(data_hash, name)
-			end
-		else
-			if data_hash.body.includes?(find)
-    		write_event(data_hash, name)
-			end
-		end
-	end
+  private def handle_find(data_hash : SyslogData, find : String, name : String, is_regex : Bool)
+    if is_regex
+      regex = Regex.new(find)
 
-	private def write_event(data_hash : SyslogData, event_name : String)
+      md = data_hash.body.match(regex)
+      if md
+        write_event(data_hash, name)
+      end
+    else
+      if data_hash.body.includes?(find)
+        write_event(data_hash, name)
+      end
+    end
+  end
+
+  private def write_event(data_hash : SyslogData, event_name : String)
     @file_manager.write_to_file(data_hash, event_name) do |file|
-			puts "Writing Event #{event_name}" if @debug
+      puts "Writing Event #{event_name}" if @debug
       handlle_output(data_hash, file)
     end
-	end
-
+  end
 end
--- src/tcp.cr
+++ src/tcp.cr
@@ -3,13 +3,14 @@ require "./processor.cr"
 require "./collectd_processor.cr"
 require "./collectd_action.cr"
 require "./action"
-class Tcp
 
-  TOTAL_FIBERS = 200
+class Tcp
+  TOTAL_FIBERS   =   200
+  GET_SIZE_LIMIT = 16000
 
   def initialize(@host : String, @port : Int32, @base_dir : String, @debug : Bool, @debug_type : Int32)
-		@action = Action.new(@base_dir, @debug)
-		@connections = 0
+    @action = Action.new(@base_dir, @debug)
+    @connections = 0
     @version = ENV["CL_VERSION"]? || "0.0.0.0"
     @processor = Processor.new
     @collectd_processor = CollectDProcessor.new
@@ -22,73 +23,87 @@ class Tcp
   end
 
   def get_socket_data(socket : TCPSocket)
-    data = nil
+    data = ""
     begin
-      data = socket.gets(true)
+      loop do
+        if txt = socket.gets(GET_SIZE_LIMIT, true)
+          data += txt.to_s
+        end
+        # Break out normally, unless we have read the ENTIRE buffer, in which case
+        # there is probably more data, so we'll 'get' again
+        break if txt && txt.size != GET_SIZE_LIMIT
+      end
       puts data.to_s if @debug_type == 1
     rescue ex
       if @debug
         puts "From Socket Address:" + socket.remote_address.to_s if socket.remote_address
-        puts ex.inspect_with_backtrace 
+        puts ex.inspect_with_backtrace
       end
     end
     return data
   end
 
-	def reader(socket : TCPSocket, processor : Processor)
-  	data = get_socket_data(socket)
+  def reader(socket : TCPSocket, processor : Processor)
+    count = 0
+    loop do
+      count += 1
+      data = get_socket_data(socket)
+      return if data.empty?
 
-    if data.to_s[0..4] == "stats"
-      p "Stats"
-      stats_response(socket)
-      return
-    end
+      if data.to_s[0..4] == "stats"
+        p "Stats"
+        stats_response(socket)
+        return
+      end
 
-    puts "Recieved: #{data}" if @debug
-    while data
-			if data && data.size > 5
-				begin
+      puts "Recieved: #{data}" if @debug
+      if data && data.size > 5
+        begin
           if data.to_s[0..7] == "collectd"
             formatted_data = @collectd_processor.process(data)
             @collectd_action.process(formatted_data)
           else
-  		  	  formatted_data = processor.process(data)
-	   				@action.process(formatted_data)
+            formatted_data = processor.process(data)
+            @action.process(formatted_data)
           end
-				rescue ex
-					p ex.message
-					p "Data:#{data}"
+        rescue ex
+          p ex.message
+          p "Data:#{data}"
           p "Remote address #{socket.remote_address.to_s}" if socket.remote_address
-   	    end
-        data = get_socket_data(socket)
-		  end
+        end
+      end
+
+      contin = socket.peek
+      # If there is no more data, or we have 1000 lines of logs, we will go ahead and break
+      # out and write them.
+      break if peek_empty?(socket) || count > 1000
     end
-	end
+  end
 
   def stats_response(socket : TCPSocket)
     data = {
-      "version" => @version,
-      "debug" => @debug,
-      "connections" => @connections,
-      "port" =>  @port,
-      "available" => TOTAL_FIBERS,
-      "open_file_count" => @action.open_file_count
+      "version"         => @version,
+      "debug"           => @debug,
+      "connections"     => @connections,
+      "port"            => @port,
+      "available"       => TOTAL_FIBERS,
+      "open_file_count" => @action.open_file_count,
     }
     p "Stats Response #{data}"
     socket.puts(data.to_json)
   end
 
-	def spawn_listener(socket_channel : Channel)
-		TOTAL_FIBERS.times do
+  def spawn_listener(socket_channel : Channel)
+    TOTAL_FIBERS.times do
       spawn do
         loop do
           begin
             socket = socket_channel.receive
             socket.read_timeout = 15
-  					@connections += 1
-  					reader(socket, @processor)
+            @connections += 1
+            reader(socket, @processor)
             socket.close
-  					@connections -= 1
+            @connections -= 1
           rescue ex
             p "Error in spawn_listener"
             p ex.message
@@ -99,15 +114,15 @@ class Tcp
   end
 
   def listen
-		ch = build_channel
-		server = TCPServer.new(@host, @port)
+    ch = build_channel
+    server = TCPServer.new(@host, @port)
 
-		spawn_listener(ch)
+    spawn_listener(ch)
     begin
-  		loop do
-    		socket = server.accept
-    		ch.send socket
-  		end
+      loop do
+        socket = server.accept
+        ch.send socket
+      end
     rescue ex
       p "Error in tcp:loop!"
       p ex.message
@@ -117,6 +132,4 @@ class Tcp
   def build_channel
     Channel(TCPSocket).new
   end
-
-
 end
