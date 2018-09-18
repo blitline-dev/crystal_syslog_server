7a6b9be9df30ac3ff07d5b1d5404c84abd93f39b
Jason <jaciones@gmail.com>
2018-09-17 19:31:36 -0700

    Added bigger buffer

--- src/tcp.cr
+++ src/tcp.cr
@@ -24,7 +24,7 @@ class Tcp
   def get_socket_data(socket : TCPSocket)
     data = nil
     begin
-      data = socket.gets('\n', 10_000, true)
+      data = socket.gets('\n', 3_000_000, true)
       puts data.to_s if @debug_type == 1
     rescue ex
       if @debug
